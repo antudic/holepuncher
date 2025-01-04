8f396f800daf84238e6b7d9bb0fefa0c4964e61f
HarKatt <haren.katten@gmail.com>
2025-01-04 04:43:02 +0100

    Help clients figure out their UDP ports

--- udp.py
+++ udp.py
@@ -9,8 +9,10 @@ def main():
 
     while True:
         _data, addr = sock.recvfrom(2048)
-        # print(_data, addr)
-        sock.sendto(b".", addr)
+
+        # we reply with the port of the request in case the NAT changes the original port of the UDP
+        # package. We don't reply with the IP since the client is listening to "0.0.0.0" either way. 
+        sock.sendto(int.to_bytes(addr[1], 2, "big"), addr)
 
 """
 import weakref
